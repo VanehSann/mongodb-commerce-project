40e0ec551a9a1f77e0cf09d79014be0416029e67
VanehSann <vanessacbsantos22@gmail.com>
2022-10-20 23:57:49 -0300

    fix: error lint

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,4 +1,5 @@
-db.produtos.updateMany({}, { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } });
+db.produtos.updateMany({},
+   { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } });
 
 db.produtos.find({}, { _id: 0, valoresNutricionais: 1, nome: 1 }); 
 
